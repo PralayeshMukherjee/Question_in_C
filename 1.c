c3332606108db179764b747bb5bde8bda8c496d2
pralayesh <rajmukherjee2807@gmail.com>
2023-12-22 00:24:23 +0530

    Given 3 numbers, find their maximum and minimum

--- 1.c
+++ 1.c
@@ -55,3 +55,62 @@ int main() {
         }
     }
 }
+// 4.Given 3 numbers, find their maximum and minimum
+// ToDo: if 1st and 2nd are same but less than 3rd 
+#include <stdio.h> 
+int main() {
+    int A,B,C;
+    printf("Input first number => \n");
+    scanf("%d", &A);
+    printf("Input second number => \n");
+    scanf("%d", &B);
+    printf("Input third number => \n");
+    scanf("%d", &C);
+    if(A>B) {
+        if(A>C) {
+            printf("A is maximum");
+        }
+    }else if(A>=B) {
+        if(A>C) {
+            printf("A and B are maximum");
+        }
+    } else if(B>C) {
+        printf("B is maximum");
+    }else if(B>=C) {
+        printf("B and C are maximum");
+    } else if(C>A) {
+        printf("C is maximum");
+    } else if(C>=A) {
+        printf("C and A are maximum");
+    }
+    printf("\n");
+    if(A<B) {
+        if(A<C) {
+            printf("A is minimum");
+        }
+    } else if(B<C) {
+        printf("B is minimum");
+    } else if(C<A) {
+        printf("C is minimum");
+    }
+    printf("\n");
+    if(A==B) {
+        if(A!=C) {
+           printf("A and B are same");
+        }else {
+            printf("A, B and C are same");
+        }
+    }else if(B==C) {
+        if(B!=A) {
+            printf("B and C are same");
+        } else{
+            printf("A, B and C are same");
+        }
+    }else if (A==C) {
+        if(A!=B) {
+            printf("A and C are same");
+        } else {
+            printf("A, B and C are same");
+        }
+    }
+}
