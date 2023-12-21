02ae13def6ae4063a42af727c0a92ca86c5d134d
pralayesh <rajmukherjee2807@gmail.com>
2023-12-21 23:04:16 +0530

    Find the factorial of a number N

--- 1.c
+++ 1.c
@@ -35,3 +35,23 @@ int main() {
         }
     }
 }
+// 3.Find the factorial of a number N.
+#include <stdio.h> 
+int main() {
+    int i,N,factorial = 1;
+    printf("Input your Natural Number =>\n");
+    scanf("%d", &N);
+    if(N==0) {
+        printf("The factorial of 0 is 1");
+    }else if(N<0) {
+        printf("This is not a valid input");
+    }else{
+        for(i=N;i>=0;i--) {
+            if(i!=0) {
+                factorial *= i;
+            }else{
+                printf("The average of the numbers is = %d\n",factorial);
+            }
+        }
+    }
+}
