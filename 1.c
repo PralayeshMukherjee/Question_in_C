10b650b020bb478ad155ee64636cdc84a8c50f47
pralayesh <rajmukherjee2807@gmail.com>
2023-12-21 22:12:54 +0530

    Find the average of the first N natural numbers

--- 1.c
+++ 1.c
@@ -16,3 +16,22 @@ int main() {
         }
     }
 }
+// 2.Find the average of the first N natural numbers.
+#include <stdio.h> 
+int main() {
+    int i,N,avg,sum = 0;
+    printf("Input your Natural Number =>\n");
+    scanf("%d", &N);
+    if(N<=0) {
+        printf("this is not a valid input");
+    }else{
+        for(i=N;i>=0;i--) {
+            if(i!=0) {
+                sum += i;
+            }else{
+                avg = (sum/N);
+                printf("The average of the numbers is = %d\n",avg);
+            }
+        }
+    }
+}
